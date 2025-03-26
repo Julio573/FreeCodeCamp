4627f177e15c0567693d1958e24028dfda07ddee
julio <julio8497@hotmail.com>
2025-03-26 11:03:09 -0300

    Second Javascript exercise

---
+++ HTML CSS JavaScript/GreetingBot/TriviaBot.js
@@ -0,0 +1,26 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Claude";
+let botLocation = "Mars";
+let favoriteLanguage = "Java";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is an OOP language.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is fun to use.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is widely used across the world.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+
+
